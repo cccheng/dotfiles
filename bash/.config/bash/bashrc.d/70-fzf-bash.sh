615ac94b92d155190c220b392941c496c57b0604
Chung-Chiang Cheng <git@cccheng.net>
2025-02-02 13:57:41 +0800

    feat(fzf): change fzf completion trigger
    - Changed the FZF_COMPLETION_TRIGGER from "~~" to "@@" in
      70-fzf-bash.sh.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- bash/.config/bash/bashrc.d/70-fzf-bash.sh
+++ bash/.config/bash/bashrc.d/70-fzf-bash.sh
@@ -7,4 +7,4 @@ FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude 
 FZF_DEFAULT_OPTS="--multi --color='dark,fg+:bright-green,pointer:bright-cyan' --preview 'less {}'"
 FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind ctrl-b:page-up,ctrl-f:page-down"
 #FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind tab:down,shift-tab:up"
-FZF_COMPLETION_TRIGGER="~~"
+FZF_COMPLETION_TRIGGER="!!"
