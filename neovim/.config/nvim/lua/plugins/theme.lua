df91870c58b3e0bba9f93c4c9db5bc3695701a41
Chung-Chiang Cheng <git@cccheng.net>
2025-02-11 15:59:31 +0800

    chore(nvim): remove tabline highlight settings
    - Remove TabLine, TabLineFill, and TabLineSel highlight settings
      from iceberg and kanagawa-paper themes.
    - These settings were causing conflicts with other plugins.
    - The changes improve the overall theme consistency.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -33,10 +33,6 @@ return {
                 -- transparent = true,
             })
             vim.cmd.colorscheme("kanagawa-paper")
-
-            vim.api.nvim_set_hl(0, "TabLine", { fg = "#9a9ca5", bg = "#1f2233", bold = false })
-            vim.api.nvim_set_hl(0, "TabLineFill", { fg = "#101218", bg = "#1f2233", bold = false })
-            vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#9a9ca5", bg = "#2b335a", bold = true })
         end
     },
 }
