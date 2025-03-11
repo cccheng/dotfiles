2145fd76d20c9965e72fb5e178174b30eff0ead7
Chung-Chiang Cheng <git@cccheng.net>
2025-03-11 16:44:39 +0800

    feat(nvim): Add mini.snippets plugin and global snippets
    - Add mini.snippets plugin configuration to load global and
        language-specific snippets.
    - Create global.json file with a snippet for current date.
    - Configure snippet expansion mapping to <C-s>.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -32,6 +32,20 @@ return {
                 },
             })
             -- require("mini.pairs").setup({})
+            require("mini.snippets").setup({
+                snippets = {
+                    -- Load custom file with global snippets first (adjust for Windows)
+                    require("mini.snippets").gen_loader.from_file("~/.config/nvim/snippets/global.json"),
+
+                    -- Load snippets based on current language by reading files from
+                    -- "snippets/" subdirectories from 'runtimepath' directories.
+                    require("mini.snippets").gen_loader.from_lang(),
+                },
+                mappings = {
+                    -- Expand snippet at cursor position. Created globally in Insert mode.
+                    expand = "<C-s>",
+                },
+            })
             require("mini.surround").setup({
                 -- Module mappings. Use `""` (empty string) to disable one.
                 mappings = {
