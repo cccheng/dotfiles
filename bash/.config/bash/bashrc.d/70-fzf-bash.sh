508cb15d6349e8deb298e3c10182dd15688ff4a7
Chung-Chiang Cheng <git@cccheng.net>
2025-03-19 14:38:40 +0800

    feat(fzf): improve fzf configuration
    -   Export `FZF_DEFAULT_COMMAND`, `FZF_DEFAULT_OPTS`, and
        `FZF_COMPLETION_TRIGGER` variables instead of using `eval`.
    -   This ensures that the variables are properly exported to the
        current shell environment.
    -   Updated comments to reflect the changes made.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- bash/.config/bash/bashrc.d/70-fzf-bash.sh
+++ bash/.config/bash/bashrc.d/70-fzf-bash.sh
@@ -3,8 +3,9 @@
 
 eval "$(fzf --bash 2>/dev/null)"
 
-FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
-FZF_DEFAULT_OPTS="--multi --color='dark,fg+:bright-green,pointer:bright-cyan' --preview 'less {}'"
+FZF_DEFAULT_OPTS="--multi --info=right --prompt='» '"
 FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind ctrl-b:page-up,ctrl-f:page-down"
-#FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind tab:down,shift-tab:up"
-FZF_COMPLETION_TRIGGER="~~"
+FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color='dark,fg:bright-black,preview-fg:white,pointer:cyan,marker:blue'"
+export FZF_DEFAULT_OPTS
+export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+export FZF_COMPLETION_TRIGGER="~~"
