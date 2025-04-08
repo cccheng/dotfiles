120779fd045f3bb978f354ea6a928aef61a56997
Chung-Chiang Cheng <git@cccheng.net>
2025-04-08 18:55:11 +0800

    feat(bash): run-cmd draft
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

---
+++ bash/.config/bash/bashrc.d/40-run-cmd.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# SPINNER="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+# SPINNER="⣾⣽⣻⢿⡿⣟⣯⣷"
+SPINNER="⣷⣯⣟⡿⢿⣻⣽⣾"
+
+stderr::disable()
+{
+    exec 3>&2           # save stderr
+    exec 2>/dev/null
+}
+
+stderr::enable()
+{
+    exec 2>&3           # restore stderr
+    exec 3>&-           # close temp fd
+}
+
+cmd::run() # $1: message, $@ command
+{
+    local idx=0 msg="$1" pid
+
+    shift; [ $# -eq 0 ] && {
+        echo "$(ansi::green)» $(ansi::reset-color)$msg"
+        return
+    }
+
+    set +m
+    ansi::hide-cursor
+
+    # Trick to suppress coproc job control message while keeping it in parent shell
+    stderr::disable
+    coproc "$@"
+    pid="$COPROC_PID"
+    stderr::enable
+
+    trap 'ansi::show-cursor; set -m; kill "$pid" 2>/dev/null; return' SIGINT SIGTERM
+    ansi::save-cursor
+    while kill -0 "$pid" 2>/dev/null; do
+        ansi::restore-cursor
+        echo -n "$(ansi::cyan)${SPINNER:$((idx++ % ${#SPINNER})):1} "
+        echo -ne "$(ansi::reset-color)$msg"
+        sleep 0.10
+    done
+
+    ansi::restore-cursor
+    ansi::show-cursor
+    set -m
+
+    if wait "$pid"; then
+        echo "$(ansi::green)» $(ansi::reset-color)$msg"
+        true
+    else
+        echo "$(ansi::red)» $(ansi::reset-color)$msg"
+        false
+    fi
+}
+
