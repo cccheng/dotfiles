ce0ce46b9cd394d06db37e5cfcaeb801b883f5a9
Chung-Chiang Cheng <git@cccheng.net>
2025-04-17 10:59:11 +0800

    chore(nvim): disable virtual lines for diagnostics
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -178,16 +178,18 @@ return {
             })
 
             vim.diagnostic.config({
-                virtual_text = {
-                    severity = {
-                        max = vim.diagnostic.severity.WARN,
-                    }
-                },
-                virtual_lines = {
-                    severity = {
-                        min = vim.diagnostic.severity.ERROR,
-                    }
-                },
+                -- virtual_text = {
+                --     severity = {
+                --         max = vim.diagnostic.severity.WARN,
+                --     }
+                -- },
+                -- virtual_lines = {
+                --     severity = {
+                --         min = vim.diagnostic.severity.ERROR,
+                --     }
+                -- },
+                virtual_text = true,
+                virtual_lines = false,
                 underline = false,
                 signs = false,
                 update_in_insert = false, -- false so diags are updated on InsertLeave
