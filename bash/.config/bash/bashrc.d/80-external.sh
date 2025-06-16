a8ae42bf92c51187d2880966c18f7fa0dfcf5d7c
Chung-Chiang Cheng <git@cccheng.net>
2025-06-16 12:16:39 +0800

    feat(mise): add neovim tool and configure status settings
    - Add neovim tool with latest version to manage Neovim installations
    - Configure status settings to improve development experience:
      - Set missing_tools to "never" to avoid notifications when other
        versions are installed
      - Enable show_env to display environment information in status output
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- bash/.config/bash/bashrc.d/80-external.sh
+++ bash/.config/bash/bashrc.d/80-external.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091
 
-if command -v mise >/dev/null; then
-    eval "$(mise activate bash)"
+if [ -s "${XDG_STATE_HOME}/nix/profile/etc/profile.d/nix.sh" ]; then
+    source "${XDG_STATE_HOME}/nix/profile/etc/profile.d/nix.sh"
 fi
 
 if [ -d "${CARGO_HOME:-$HOME/.cargo}/bin" ]; then
     add_path "${CARGO_HOME:-$HOME/.cargo}/bin"
 fi
 
-if [ -s "${rvm_path:-$HOME/.rvm}/scripts/rvm" ]; then
-    source "${rvm_path:-$HOME/.rvm}/scripts/rvm"
-fi
-
-if [ -s "${XDG_STATE_HOME}/nix/profile/etc/profile.d/nix.sh" ]; then
-    source "${XDG_STATE_HOME}/nix/profile/etc/profile.d/nix.sh"
+if command -v mise >/dev/null; then
+    eval "$(mise activate bash)"
 fi
 
 # Homebrew ruby
