ccccc6865212612d0a2eb69c86b11896f1db1164
Chung-Chiang Cheng <git@cccheng.net>
2025-07-10 18:00:32 +0800

    feat(snacks): enable bigfile plugin
    Enable the bigfile plugin in snacks.nvim configuration to improve
    performance when working with large files. This plugin automatically
    detects large files and disables certain features that might cause
    performance issues.
    
    Signed-off-by: Chung-Chiang Cheng <git@cccheng.net>

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,10 @@
 return {
     "folke/snacks.nvim",
+    event = "VeryLazy",
     opts = {
+        bigfile = {
+            enabled = true,
+        },
         indent = {
             enabled = true,
         },
