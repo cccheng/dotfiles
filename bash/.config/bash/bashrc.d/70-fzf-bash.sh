ccc15886ea4a0de38460569f622abd219bda6513
Chung-Chiang Cheng <git@cccheng.net>
2025-09-23 11:47:37 +0800

    feat(bash): improve fzf color scheme for better visibility
    Signed-off-by: Chung-Chiang Cheng <git@cccheng.net>

--- bash/.config/bash/bashrc.d/70-fzf-bash.sh
+++ bash/.config/bash/bashrc.d/70-fzf-bash.sh
@@ -5,7 +5,7 @@ eval "$(fzf --bash 2>/dev/null)"
 
 FZF_DEFAULT_OPTS="--multi --info=right --prompt='» '"
 FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --bind ctrl-b:page-up,ctrl-f:page-down"
-FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color='dark,fg:bright-black,preview-fg:white,pointer:cyan,marker:blue'"
+FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color='dark,fg:bright-black,current-fg:bright-green,pointer:green,marker:blue'"
 export FZF_DEFAULT_OPTS
 export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
 export FZF_COMPLETION_TRIGGER="~~"
