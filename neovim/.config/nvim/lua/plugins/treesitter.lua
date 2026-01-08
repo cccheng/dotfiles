cccef95aac32f8e8ae94437291de612062b3f8f8
Chung-Chiang Cheng <git@cccheng.net>
2026-01-08 18:30:24 +0800

    feat(neovim): treesitter, enhance textobjects configuration and keymaps
    - Add init function to disable built-in ftplugin mappings to prevent
      conflicts with treesitter-textobjects keybindings
    - Enhance select textobjects with additional comments explaining
      configuration options (lookahead, selection_modes,
      include_surrounding_whitespace)
    - Update selection_modes: change function to linewise (V) and class to
      blockwise (<c-v>) for more appropriate text object behavior
    - Add new textobject keymaps:
      - a<C-s>/Language scope for local scope selection
      - a<M-c>/i<M-c> for conditional outer/inner selection
    - Add swap functionality with keymaps:
      - <LEADER>ln to swap parameters forward
      - <LEADER>lp to swap parameters backward
    - Reorganize move keymaps for consistency:
      - Change comment navigation from ]C/[C to ]<M-C>/[<M-C>
      - Reassign ]C/[C for class navigation (previously ]c/[c)
    - Implement custom config function to programmatically set up keymaps
      for select, swap, and move operations, replacing the default lazy
      loading behavior for better control
    
    Signed-off-by: Chung-Chiang Cheng <git@cccheng.net>

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -57,25 +57,66 @@ return {
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
         },
+        init = function()
+            -- Disable entire built-in ftplugin mappings to avoid conflicts.
+            -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+            vim.g.no_plugin_maps = true
+
+            -- Or, disable per filetype (add as you like)
+            -- vim.g.no_python_maps = true
+            -- vim.g.no_ruby_maps = true
+            -- vim.g.no_rust_maps = true
+            -- vim.g.no_go_maps = true
+        end,
         opts = {
             select = {
+                -- Automatically jump forward to textobj, similar to targets.vim
                 lookahead = true,
+                -- You can choose the select mode (default is charwise 'v')
+                --
+                -- Can also be a function which gets passed a table with the keys
+                -- * query_string: eg '@function.inner'
+                -- * method: eg 'v' or 'o'
+                -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                -- mapping query_strings to modes.
                 selection_modes = {
-                    ["@class.outer"] = "V",
-                    ["@function.outer"] = "v",
-                    ["@parameter.outer"] = "v",
+                    ["@parameter.outer"] = "v", -- charwise
+                    ["@function.outer"] = "V", -- linewise
+                    ["@class.outer"] = "<c-v>", -- blockwise
                 },
+                -- If you set this to `true` (default is `false`) then any textobject is
+                -- extended to include preceding or succeeding whitespace. Succeeding
+                -- whitespace has priority in order to act similarly to eg the built-in
+                -- `ap`.
+                --
+                -- Can also be a function which gets passed a table with the keys
+                -- * query_string: eg '@function.inner'
+                -- * selection_mode: eg 'v'
+                -- and should return true of false
                 include_surrounding_whitespace = true,
                 keymaps = {
                     select_textobject = {
+                        ["a<C-s>"] = { query = "@local.scope", query_group = "locals", desc = "TSTextObject | Language Scope" },
+                        ["a<M-c>"] = { query = "@conditional.outer", desc = "TSTextObject | Around Condition" },
                         ["aC"] = { query = "@comment.outer", desc = "TSTextObject | Around Comment" },
                         ["aa"] = { query = "@parameter.outer", desc = "TSTextObject | Outer Argument/Parameter" },
                         ["ac"] = { query = "@class.outer", desc = "TSTextObject | Around Class" },
                         ["af"] = { query = "@function.outer", desc = "TSTextObject | Around Function/Method" },
+                        ["i<M-c>"] = { query = "@conditional.inner", desc = "TSTextObject | Inner Condition" },
                         ["iC"] = { query = "@comment.outer", desc = "TSTextObject | Inner Comment" },
                         ["ia"] = { query = "@parameter.inner", desc = "TSTextObject | Inner Argument/Parameter" },
                         ["ic"] = { query = "@class.inner", desc = "TSTextObject | Inner Class" },
                         ["if"] = { query = "@function.inner", desc = "TSTextObject | Inner Function/Method" },
+                    },
+                },
+            },
+            swap = {
+                keymaps = {
+                    swap_next = {
+                        ["<lEADER>ln"] = { query = "@parameter.inner", desc = "TSTextObject | Swap Params Next" },
+                    },
+                    swap_previous = {
+                        ["<LEADER>lp"] = { query = "@parameter.inner", desc = "TSTextObject | Swap Params Previous" },
                     },
                 },
             },
@@ -86,8 +127,8 @@ return {
                         ["]<C-l>"] = { query = "@loop.*", desc = "TSTextObject | Next Loop Start" },
                         ["]<C-s>"] = { query = "@scope", query_group = "locals", desc = "TSTextObject | Next Scope Start" },
                         ["]<M-c>"] = { query = "@conditional.outer", desc = "TSTextObject | Next Condition" },
-                        ["]C"] = { query = "@comment.outer", desc = "TSTextObject | Next Comment Start" },
-                        ["]c"] = { query = "@class.outer", desc = "TSTextObject | Next Class Start" },
+                        ["]<M-C>"] = { query = "@comment.outer", desc = "TSTextObject | Next Comment Start" },
+                        ["]C"] = { query = "@class.outer", desc = "TSTextObject | Next Class Start" },
                         ["]f"] = { query = "@function.outer", desc = "TSTextObject | Next Function/Method Start" },
                         ["]r"] = { query = "@return.outer", desc = "TSTextObject | Next Return" },
                         ["]F"] = { query = "@fold", query_group = "folds", desc = "TSTextObject | Next Fold Start" },
@@ -96,8 +137,8 @@ return {
                         ["[<C-l>"] = { query = "@loop.*", desc = "TSTextObject | Previous Loop Start" },
                         ["[<C-s>"] = { query = "@scope", query_group = "locals", desc = "TSTextObject | Previous Scope Start" },
                         ["[<M-c>"] = { query = "@conditional.outer", desc = "TSTextObject | Previous Condition" },
-                        ["[C"] = { query = "@comment.outer", desc = "TSTextObject | Previous Comment Start" },
-                        ["[c"] = { query = "@class.outer", desc = "TSTextObject | Previous Class Start" },
+                        ["[<M-C>"] = { query = "@comment.outer", desc = "TSTextObject | Previous Comment Start" },
+                        ["[C"] = { query = "@class.outer", desc = "TSTextObject | Previous Class Start" },
                         ["[f"] = { query = "@function.outer", desc = "TSTextObject | Previous Function/Method Start" },
                         ["[r"] = { query = "@return.outer", desc = "TSTextObject | Previous Return" },
                         ["[F"] = { query = "@fold", query_group = "folds", desc = "TSTextObject | Previous Fold Start" },
@@ -105,5 +146,32 @@ return {
                 },
             },
         },
+        config = function(_, opts)
+            require("nvim-treesitter-textobjects").setup(opts)
+
+            for select_func, select_values in pairs(opts.select.keymaps) do
+                for select_keymap, select_keymap_value in pairs(select_values) do
+                    vim.keymap.set({ "x", "o" }, select_keymap, function()
+                        require("nvim-treesitter-textobjects.select")[select_func](select_keymap_value["query"], select_keymap_value["query_group"])
+                    end, { desc = select_keymap_value["desc"], silent = true, })
+                end
+            end
+
+            for swap_func, swap_values in pairs(opts.swap.keymaps) do
+                for swap_keymap, swap_keymap_value in pairs(swap_values) do
+                    vim.keymap.set("n", swap_keymap, function()
+                        require("nvim-treesitter-textobjects.swap")[swap_func](swap_keymap_value["query"], swap_keymap_value["query_group"])
+                    end, { desc = swap_keymap_value["desc"], silent = true, })
+                end
+            end
+
+            for move_func, move_values in pairs(opts.move.keymaps) do
+                for move_keymap, move_keymap_value in pairs(move_values) do
+                    vim.keymap.set({ "n", "x", "o" }, move_keymap, function()
+                        require("nvim-treesitter-textobjects.move")[move_func](move_keymap_value["query"], move_keymap_value["query_group"])
+                    end, { desc = move_keymap_value["desc"], silent = true, })
+                end
+            end
+        end,
     },
 }
