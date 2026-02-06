cccf7d50eb85e4ef51ebc769a64bce8d7a96bf95
Chung-Chiang Cheng <git@cccheng.net>
2026-02-06 09:39:25 +0800

    feat(neovim): add gitlineage plugin for git line history
    Add gitlineage.nvim plugin to view git history for selected lines.
    This enables tracking line-level changes across commits with an
    intuitive interface.
    
    - Add lionyxml/gitlineage.nvim plugin with lazy loading
    - Configure visual mode keybinding <LEADER>gl for line history
    - Add sindrets/diffview.nvim as optional dependency for diff viewing
    - Include commented configuration options for customization reference
    
    Signed-off-by: Chung-Chiang Cheng <git@cccheng.net>

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -41,6 +41,29 @@ return {
         end
     },
     {
+        "lionyxml/gitlineage.nvim",
+        lazy = true,
+        dependencies = {
+            "sindrets/diffview.nvim", -- optional, for open_diff feature
+        },
+        keys = {
+            { "<LEADER>gl", mode = {"v"}, function() require("gitlineage").show_history() end, desc = "Git line history" },
+        },
+        config = function()
+            require("gitlineage").setup({
+                -- split = "auto",         -- "vertical", "horizontal", or "auto"
+                -- keymap = "<leader>gl",  -- set to nil to disable default keymap
+                -- keys = {
+                --     close = "q",        -- set to nil to disable
+                --     next_commit = "]c", -- set to nil to disable
+                --     prev_commit = "[c", -- set to nil to disable
+                --     yank_commit = "yc", -- set to nil to disable
+                --     open_diff = "<CR>", -- set to nil to disable (requires diffview.nvim)
+                -- },
+            })
+        end
+    },
+    {
         "FabijanZulj/blame.nvim",
         lazy = true,
         keys = {
