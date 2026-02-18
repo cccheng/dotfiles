ccc5700521c1ef97823c373e3f54d6c48004fa49
Chung-Chiang Cheng <git@cccheng.net>
2026-02-19 00:17:42 +0800

    feat(neovim): enhance filetype-specific configurations
    - Add indentation settings to gitcommit ftplugin
      * Set tabstop, shiftwidth, and softtabstop to 2 spaces
      * Enable expandtab for consistent spacing in commit messages
    
    - Create man page ftplugin for improved viewing experience
      * Disable wrap, list, line numbers, and spell checking
      * Hide tabline and sign column for cleaner display
      * Add <LEADER>o keymap to show table of contents
    
    - Create quickfix ftplugin to enable line wrapping
      * Set wrap option to true for better readability of long lines
    
    Signed-off-by: Chung-Chiang Cheng <git@cccheng.net>

---
+++ neovim/.config/nvim/after/ftplugin/man.lua
@@ -0,0 +1,12 @@
+vim.wo.wrap = false
+vim.opt_local.wrap = false
+vim.opt_local.showtabline = 0
+vim.opt_local.list = false
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+vim.opt_local.signcolumn = "no"
+vim.opt_local.spell = false
+
+vim.keymap.set("n", "<LEADER>o", function()
+    require("man").show_toc()
+end, { desc = "Man: Show table of content" })
