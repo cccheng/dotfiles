cccace18d354c06fcadaeb2b59adaf0b93c49b87
Chung-Chiang Cheng <git@cccheng.net>
2026-02-26 23:01:12 +0800

    feat(neovim): treesitter, add function move
    Signed-off-by: Chung-Chiang Cheng <git@cccheng.net>

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -105,17 +105,24 @@ return {
             -- select
             vim.keymap.set({ "x", "o" }, "af", function()
                 require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
-            end)
+            end, { desc = "function" })
             vim.keymap.set({ "x", "o" }, "if", function()
                 require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
-            end)
+            end, { desc = "innter function" })
             vim.keymap.set({ "x", "o" }, "ac", function()
                 require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
-            end)
+            end, { desc = "class" })
             vim.keymap.set({ "x", "o" }, "ic", function()
                 require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
-            end)
+            end, { desc = "inner class" })
 
+            -- move
+            vim.keymap.set({ "n", "x", "o" }, "]f", function()
+                require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+            end, { desc = "Next function" })
+            vim.keymap.set({ "n", "x", "o" }, "[f", function()
+                require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+            end, { desc = "Previous function" })
         end,
     },
 }
