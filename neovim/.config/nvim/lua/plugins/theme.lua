ccc19bd1f2e708b4b10b191a11a0029b1ff4a1e0
Chung-Chiang Cheng <git@cccheng.net>
2026-03-25 11:39:23 +0800

    chore(neovim): update colorscheme
    Signed-off-by: Chung-Chiang Cheng <git@cccheng.net>

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -6,7 +6,7 @@ return {
         -- "cocopon/iceberg.vim",
         "oahlen/iceberg.nvim",
         lazy = selected_theme ~= "iceberg",
-        priority = 1002,
+        priority = 1000,
         config = function()
             vim.cmd.colorscheme("iceberg")
             vim.api.nvim_set_hl(0, "TabLine", { fg = "#9a9ca5", bg = "#1f2233", bold = false })
@@ -34,7 +34,7 @@ return {
         config = function()
             require("kanagawa-paper").setup({
                 dimInactive = false,
-                -- transparent = true,
+                transparent = true,
             })
             vim.cmd.colorscheme("kanagawa-paper")
             vim.api.nvim_set_hl(0, "TabLine", { fg = "#9a9ca5", bg = "#1f2233", bold = false })
